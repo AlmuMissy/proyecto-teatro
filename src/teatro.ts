37dd2ea3e0f522a8cf9f8430c3e5617d4049e1a2
Almudena <almufullstack@gmail.com>
2022-07-06 16:02:13 +0200

    no message

---
+++ src/teatro.ts
@@ -0,0 +1,8 @@
+export class Teatro {
+   
+  verProgramacionActual() {
+      console.log('Hoy representamos La cena de los idiotas, género: Comedia, Duración: 95');    
+      console.log('En C/Sol 45, local de 300 metros, con 2 accesos');
+      console.log('Precio: 30.0');                    
+  }
+}
